9693b00dfafd52e7594547614eb3c09347a0c86e
Mugaba Ashiraf <hanmas669@gmail.com>
2026-07-18 12:25:01 +0300

    Added Data models

--- spaces/models.py
+++ spaces/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+import random, string
 # Create your models here.
+
+def generate_pin():
+    """A method for generating 4 Digit space pins for access"""
+    return ''.join(random.choices(string.digits, k=4))
+
+class Space(models.Model):
+    name = models.CharField(max_length=100, help_text="space name")
+    host_phone = models.CharField()
+    pin = models.CharField(max_length=4, unique=True, default=generate_pin)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ("name", "host_phone")
+    
+    def __str__(self):
+        return f"YoSpace{self.name} is created and you PIN is ({self.pin})"
+
+class SpaceInvitee(models.Model):
+    space = models.ForeignKey(Space, related_name="invitees", on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    invited_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ("space", "phone_number") # Prevents duplicate SMS
+
+class ActiveSpaceParticipant(models.Model):
+    space = models.ForeignKey(Space, related_name="active_participants", on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    call_session_id = models.CharField(max_length=100, blank=True, null=True)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ("space", "phone_number")
+    
+    def masked_phone(self):
+        return f"****{self.phone_number[-4:]}"
--- spaces/views.py
+++ spaces/views.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import xml.sax.saxutils as sx
+import logging
+from .models import *
+import africastalking
 
-# Configure Africastalking credentials
+logger = logging.getLogger("yospaces")
+
+# Configure AfricasTalking credentials
 
 # Configure Gemini Credentials
 
@@ -13,16 +19,40 @@ def ussd_callback(request):
     phone_number = request.POST.get("phoneNumber", "")
     text = request.POST.get("text", "")
     
+    input_parts = text.split("*") if text else []
+    
     response = ""
+    
     if text == "":
         response = "CON YoSpaces\n"
-        response += "1. Join a Space\n"
-        response += "2. Host a Space\n"
+        response += "1. Host a Space\n"
+        response += "2. Join a Space\n"
         response += "3. Browse a Space\n"
         response += "4. About YoSpace\n"
         response += "5. Exit\n"
     elif text == "1":
-        response = "END Joining a space is still under development. We are sorry for any interruptions."
+        response = "CON Enter a short name for your Space:(e.g FamilyTalks)"
+    elif len(input_parts) == 2 and input_parts[0] == "1":
+        space_name = input_parts[1]
+        Space.objects.get_or_create(name=space_name, host_phone=phone_number)
+        
+        # send and sms or continue to add memebers phonenumbers and save them to the database
+        response = f"CON {space_name} Dashboard\n"
+        response += "1. Manage Members\n"
+        response += "2. Manage Space\n"
+        response += "3. Go Live\n"
+    elif len(input_parts) == 3 and input_parts[0] == "1" and input_parts[2] == "1":
+    # elif text == "1*1":
+        response = "CON Manage Members\n"
+        response += "1. Add Member\n"
+        response += "2. Remove Member\n"
+        response += "3. View Members\n"
+        
+    elif text == "1*2":
+        space_name = Space.name
+        response == f"CON {space_name}\n"
+        response += "1. Edit Space\n"
+        response += "2. Go Live\n"
     elif text == "2":
         response = "END Hosting a space is still under development. We are sorry for any interruptions."
     elif text == "3":
